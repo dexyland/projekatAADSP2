7f0e3e2ada8e9cae1bd7255a15391d670898493d
dexyland <dejan.martinov94@yahoo.com>
2018-03-09 16:16:02 +0100

    testing in progress

--- TestProjekat.bat
+++ TestProjekat.bat
@@ -7,7 +7,7 @@
 cd projekat_model3\SULDWavTemplate\
 
 
-c:\CirrusDSP\bin\cmdline_simulator.exe -project SimulatorConfigurationTemp.sbr -max_cycles 50000000
+c:\CirrusDSP\bin\cmdline_simulator.exe -project SimulatorConfigurationTemp.sbr -max_cycles 10000000
 
 
 cd ..
@@ -17,6 +17,6 @@ cd ..
 
 
 :"..//tools//PCMCompare.exe" OutStreams//speech_out_21.wav OutStreams//speech_out_31.wav 2> OutCmp//speech_Model2_vs_Model3.txt
-"tools//PCMCompare.exe" OutStreams//Freq_sweep_model2.wav OutStreams//Freq_sweep_model3.wav 2> OutCmp//Freq_sweep_Model2_vs_Model3.txt
+"tools//PCMCompare.exe" OutStreams//Freq_sweep_model2.wav OutStreams//Freq_sweep_model3.wav 2> OutCmp//Freq_sweep_Model2_vs_Model3.txt -w
 
 pause
